10c2f61fe17f03f37483c76e29f8fc6ace8023eb
Meenal Garg <meenalgarg0305@gmail.com>
2023-05-29 22:42:08 +0530

    loading web component

--- src/lib/Folder.svelte
+++ src/lib/Folder.svelte
@@ -1,1 +1,33 @@
-<folder-component></folder-component>
+<script>
+    import { onMount } from 'svelte';
+  
+    let is_user_authenticated = false;
+    let isWebComponentLoaded = false;
+  
+    function loadWebComponent() {
+      const script = document.createElement('script');
+      script.src = '/src/assets/folder-web-component.js';
+      script.async = true;
+      script.onload = () => {
+        isWebComponentLoaded = true;
+      };
+  
+      document.body.appendChild(script);
+    }
+  
+    onMount(() => {
+        if(localStorage.getItem('username') && 'admin' === localStorage.getItem('username')){
+            is_user_authenticated = true;
+        }
+        if (!isWebComponentLoaded) {
+            loadWebComponent();
+        }
+    });
+  </script>
+  
+  {#if isWebComponentLoaded}
+    <folder-component {is_user_authenticated}></folder-component>
+  {:else}
+    <p>Loading...</p>
+  {/if}
+  
